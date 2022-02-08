b93de1cfe768daa3dbae1ce970c5ac9eaea4c1c9
ngocmy <phamngocmy177@gmail.com>
2022-02-08 16:54:01 +0700

    fix deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,10 @@
 async function main() {
   const StakingContract = await ethers.getContractFactory("StakingRewards");
-  const stakingContract = await StakingContract.deploy("StakingRewards");
+  //uniswap token rewards
+  const stakingContract = await StakingContract.deploy(
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+    "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984"
+  );
 
   console.log("My Contract deployed to:", stakingContract.address);
 }
